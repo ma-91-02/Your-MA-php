a8f386ac97af61121f8d8091424c0ff63f45bc98
mahmood <mahmood@mgt.eu>
2023-01-08 23:51:16 +0300

    edit arabic css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,14 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" >
+@if (app()->getLocale() == 'ar')
+<?php
+$dir = 'style=direction:rtl;'
+?>
+@else
+<?php
+$dir='style=direction:ltr;'
+?>
+@endif
 
 <head>
   <meta charset="utf-8">
@@ -13,8 +22,10 @@
   <!-- main css  -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.rtl.min.css" integrity="sha384-+qdLaIRZfNu4cVPK/PxJJEy0B0f3Ugv8i482AKY7gwXwhaCroABd086ybrVKTa0q" crossorigin="anonymous">
-  <link rel="stylesheet" href="resources/css/normalize.css" />
+  <link rel="stylesheet" href="{{ asset('resources/css/normalize.css') }}" />
   <link rel="stylesheet" href="{{ asset('resources/css/yourMA.css') }}" />
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}" />
+  <link rel="stylesheet" href="{{ asset('css/yourMA.css') }}" />
   <!-- Font Awesome -->
   <link rel="stylesheet" href="resources/css/all.min.css" />
   <!-- Fonts -->
@@ -424,7 +435,7 @@
   </style>
 </head>
 
-<body class="antialiased">
+<body class="antialiased" {{$dir}}>
 
   <!-- Start this code for login for future -->
 
